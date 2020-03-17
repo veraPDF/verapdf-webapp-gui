70c54494834c123543b3b66e0ec5a15acc408964
Igor Poplavsky <igor.poplavsky@duallab.com>
2020-03-17 09:53:19 +0300

    #3: Added redux-thunk for async actions

--- src/store/rootStore.js
+++ src/store/rootStore.js
@@ -1,6 +1,7 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
 
 export default function configureStore() {
-    return createStore(rootReducer);
+    return createStore(rootReducer, applyMiddleware(thunk));
 }

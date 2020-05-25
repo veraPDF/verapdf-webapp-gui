603bebbf7b9c7e9c3a72d368e342cf0c28e1439b
Igor Poplavsky <igor.poplavsky@duallab.com>
2020-05-25 20:53:09 +0300

    #33: Added rule/check tree for Inspect page

--- src/store/job/result/selectors.js
+++ src/store/job/result/selectors.js
@@ -4,9 +4,11 @@ export const getResult = state => state.taskResult;
 
 export const hasResult = createSelector(getResult, result => result != null);
 
-export const getResultDetails = createSelector(getResult, result => result.details);
+export const getResultDetails = createSelector(getResult, result => result?.details);
 
 export const getResultSummary = createSelector(getResultDetails, ({ passedChecks, failedChecks }) => ({
     passedChecks,
     failedChecks,
 }));
+
+export const getRuleSummaries = createSelector(getResultDetails, ({ ruleSummaries }) => [...ruleSummaries]);

b282170d29f60c04e4d29fdcafd911786212ac55
Igor Poplavsky <igor.poplavsky@duallab.com>
2020-06-03 19:22:56 +0300

    #68: Added support of pages
    Filtering out metadata and unrecognized checks;
    #50 and #59 were fixed as unrecognized;

--- src/components/layouts/pages/inspect/Inspect.js
+++ src/components/layouts/pages/inspect/Inspect.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Redirect, useParams } from 'react-router-dom';
@@ -7,6 +7,7 @@ import _ from 'lodash';
 
 import AppPages from '../../../AppPages';
 import { JOB_STATUS, TASK_STATUS } from '../../../../store/constants';
+import { lockApp, unlockApp } from '../../../../store/application/actions';
 import { getJobStatus, getTaskStatus } from '../../../../store/job/selectors';
 import Toolbar from './toolbar/Toolbar';
 import Tree from './tree/Tree';
@@ -17,10 +18,22 @@ import './Inspect.scss';
 const { PUBLIC_URL } = process.env;
 pdfjs.GlobalWorkerOptions.workerSrc = `${PUBLIC_URL}/pdf.worker.js`;
 
-function Inspect({ jobStatus, taskStatus }) {
+function Inspect({ jobStatus, taskStatus, lockApp, unlockApp }) {
     const { id: jobId } = useParams();
     const [pdfName, setPdfName] = useState('');
     const [selectedCheck, setSelectedCheck] = useState(null);
+    const [errorsMap, setErrorsMap] = useState({});
+    const onDocumentReady = useCallback(
+        eMap => {
+            setErrorsMap(eMap);
+            unlockApp();
+        },
+        [unlockApp]
+    );
+
+    useEffect(() => {
+        lockApp();
+    }, [lockApp]);
 
     if (jobStatus === JOB_STATUS.NOT_FOUND) {
         return <Redirect to={AppPages.NOT_FOUND} />;
@@ -32,8 +45,13 @@ function Inspect({ jobStatus, taskStatus }) {
     return (
         <section className="inspect">
             <Toolbar name={pdfName} />
-            <Tree selectedCheck={selectedCheck} setSelectedCheck={setSelectedCheck} />
-            <PdfDocument selectedCheck={selectedCheck} setSelectedCheck={setSelectedCheck} setPdfName={setPdfName} />
+            <Tree selectedCheck={selectedCheck} setSelectedCheck={setSelectedCheck} errorsMap={errorsMap} />
+            <PdfDocument
+                selectedCheck={selectedCheck}
+                setSelectedCheck={setSelectedCheck}
+                setPdfName={setPdfName}
+                onDocumentReady={onDocumentReady}
+            />
         </section>
     );
 }
@@ -41,6 +59,8 @@ function Inspect({ jobStatus, taskStatus }) {
 Inspect.propTypes = {
     jobStatus: PropTypes.oneOf(_.values(JOB_STATUS)).isRequired,
     taskStatus: PropTypes.oneOf(_.values(TASK_STATUS)),
+    lockApp: PropTypes.func.isRequired,
+    unlockApp: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => {
@@ -50,4 +70,11 @@ const mapStateToProps = state => {
     };
 };
 
-export default connect(mapStateToProps)(Inspect);
+const mapDispatchToProps = dispatch => {
+    return {
+        lockApp: () => dispatch(lockApp()),
+        unlockApp: () => dispatch(unlockApp()),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Inspect);

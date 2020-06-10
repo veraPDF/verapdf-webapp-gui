59d7dfe22de3d42a2b1b6d76a593491726bd1b50
Igor Poplavsky <igor.poplavsky@duallab.com>
2020-06-10 16:44:18 +0300

    #97: Fixed long document title in dropzone and summary
    Changed tests

--- src/__tests__/integration/upload.test.js
+++ src/__tests__/integration/upload.test.js
@@ -37,7 +37,7 @@ describe('Upload', () => {
         integrationTest(async (store, component) => {
             await storeFile(component, store);
 
-            expect(getDropzoneText(component)).toEqual(`${TEST_FILE.name} - ${TEST_FILE.size}`);
+            expect(getDropzoneText(component)).toEqual(`${TEST_FILE.name}${TEST_FILE.size}`);
             expect(getNextStepButton(component).props().disabled).toBeFalsy();
         })
     );
@@ -71,7 +71,7 @@ describe('Upload', () => {
             moveNext(component);
             moveBack(component);
 
-            expect(getDropzoneText(component)).toEqual(`${TEST_FILE.name} - ${TEST_FILE.size}`);
+            expect(getDropzoneText(component)).toEqual(`${TEST_FILE.name}${TEST_FILE.size}`);
         })
     );
 });

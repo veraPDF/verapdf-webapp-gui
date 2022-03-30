a11709b02f339433ea0d3c0abbd79e8d1b1136f3
Victoria Bezrukova <victoria.bezrukova@duallab.com>
2022-03-30 08:23:41 +0300

    Add script to restart docker containers from github images

---
+++ .bin/restart-docker-remote.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+script_path=$(realpath "$0")
+script_dir=$(dirname "$script_path")
+project_root=$(dirname "$script_dir")
+
+# shellcheck disable=SC1090
+. "$script_dir"/set-env.sh
+
+# passing --clean should remove all docker volumes meaning purging all the data and cleaning orphan containers
+down_opts="";
+if [ "$1" == "--clean" ]
+  then
+    down_opts="$down_opts --volumes --remove-orphans"
+fi
+
+docker-compose -f "$project_root"/server/.docker/docker-compose.yml -f "$project_root"/.docker/docker-compose.staging.yml down $down_opts
+docker-compose -f "$project_root"/server/.docker/docker-compose.yml -f "$project_root"/.docker/docker-compose.staging.yml up -d --build

9079827f0794bef40bcb41d291f570b042da22a2
Elizaveta Selyukovich <elizaveta.selyukovich@duallab.com>
2022-08-30 12:06:18 +0300

    Support validation report for electron-based app

--- src/common/store/job/actions.js
+++ src/common/store/job/actions.js
@@ -36,12 +36,13 @@ export const validate = (completedSteps = []) => async (dispatch, getState) => {
 
 export const loadValidationResult = async (dispatch, getState) => {
     const taskStatus = getTaskStatus(getState());
+    const userAgent = navigator.userAgent.toLowerCase();
     if (taskStatus === TASK_STATUS.ERROR) {
         throw new Error(getTaskErrorMessage(getState()));
     }
     const resultFileId = getTaskResultId(getState());
     const validationResult = await FileService.getFileContent(resultFileId);
-    dispatch(setResult(validationResult));
+    dispatch(setResult(userAgent.indexOf(' electron/') > -1 ? validationResult.validationResult : validationResult));
 };
 
 const createStep = (key, percentage = 0, stepFn) => async (dispatch, getState, completedSteps) => {

369c51d9d312e6175a9961d705c11db25e0a087a
Pavel <75898907+GribovskyPavel@users.noreply.github.com>
2023-11-10 16:59:08 +0300

    Update react import

--- src/common/components/shared/dialog/Dialog.js
+++ src/common/components/shared/dialog/Dialog.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
@@ -14,7 +14,7 @@ import Button from '../button/Button';
 import './Dialog.scss';
 
 function Dialog({ title, tags, actions, open, onClose, children }) {
-    const tagsArray = React.useMemo(() => {
+    const tagsArray = useMemo(() => {
         return tags.map((tag, index) => {
             return (
                 <li key={index}>
